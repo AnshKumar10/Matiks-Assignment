a2d408e057f74af88f7487a6b4212068470758ac
Ansh Kumar <anshkumar8710@gmail.com>
2026-01-20 17:17:18 +0530

    updated api url

--- leaderboard-ui/app/api.ts
+++ leaderboard-ui/app/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from "axios";
 import { LeaderboardUser } from "./types";
 
-const API_BASE = "http://localhost:8080";
+const API_BASE = "https://matiks-assignment-production.up.railway.app";
 
 export const getLeaderboard = async (
   limit: number = 1000,
